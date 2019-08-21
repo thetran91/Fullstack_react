5245956549206034d189b41354218f6100f6e877
The Tran <eg.tranxuanthe@gmail.com>
2019-08-21 10:36:46 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send({ Hi: 'There' });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
