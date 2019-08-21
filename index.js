b49308ba7cd6102f2b63ea79ebabf0edfa51b7ee
The Tran <eg.tranxuanthe@gmail.com>
2019-08-21 10:56:18 +0700

    change greeting

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({ Bye: 'The' });
+  res.send({ Hi: 'The Tran' });
 });
 
 const PORT = process.env.PORT || 5000;
