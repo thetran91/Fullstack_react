05d160ea621580e42f2266c773cb0c65aa0ade59
The Tran <eg.tranxuanthe@gmail.com>
2019-08-21 18:04:12 +0700

    passport finished

--- index.js
+++ index.js
@@ -1,9 +1,41 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+/*Lay google oauth key */
+const keys = require('./config/keys');
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ Hi: 'The Tran' });
-});
+/* Su dung Strategy */
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      /* URL The route user wiil be sent to after they allow login thier email */
+      callbackURL: '/auth/google/callback'
+    },
+    /* Tra ve token cua user sau khi dang nhap */
+    (accessToken, refreshToken, profile, done) => {
+      console.log('accessToken', accessToken);
+      console.log('refreshToken', refreshToken);
+      console.log('profile', profile);
+    }
+  )
+);
+/* Tell google that we have user's token to access thier account */
+app.get(
+  '/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
+    /* Scope: thong bao google biet chung ta muon lay thong tin gi, google co nhieu thuoc tinh khac ngoai 2 thuoc tinh tren */
+  })
+);
+app.get('/auth/google/callback', passport.authenticate('google'));
 
-const PORT = process.env.PORT || 5000;
+app.get('/', (req, res) => {
+  res.send('Hello');
+});
+const PORT = process.env.PORT || 3000;
 app.listen(PORT);
---
+++ readme.md
@@ -0,0 +1,12 @@
+# Day 1:
+
+- Install express vao tao code base
+- Xem chart image
+- Hoc cach deploy qua heroku cli
+  cmd: heroku create, heroku login, heroku open, git remote add heroku,git push heroku master (Bai 14,15)
+- Hoc cach dung passportjs cho Oauth.
+  cmd: npm install passport passport-google-oauth20
+  Cau hinh Google Oauth Api tree google console api
+  http://localhost:5000/demo
+  Client ID
+  Client Secrete: luu tron config/keys (Bai 19,20...)
